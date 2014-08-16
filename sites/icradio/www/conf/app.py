6f93e7036afca2ef541a108c2f39e657f0ebb5b2
Luke Granger-Brown <git@lukegb.com>
2014-08-15 21:58:19 -0400

    Corrects config

--- sites/icradio/www/conf/app.py
+++ sites/icradio/www/conf/app.py
@@ -8,8 +8,8 @@ import sys
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'icradioweb.settings'
 
-sys.path.append('/srv/www/icradio/www/repo/code/modules')
-sys.path.append('/srv/www/icradio/www/repo/code/modules/icradioweb/mainapp')
+sys.path.append('/srv/www/icradio/www/code/modules')
+sys.path.append('/srv/www/icradio/www/code/modules/icradioweb/mainapp')
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
